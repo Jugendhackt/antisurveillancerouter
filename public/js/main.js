199c6702658bbd66fdf8039700d4ee15f0e46f26
Jake <pygamer138@gmail.com>
2020-03-20 17:15:16 +0100

    Finish skeleton

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+const checkResize = () => {
+    if ($('#search_input')[0].value !== '') {
+        $('#search_bar').addClass('high');
+    } else {
+        try {
+            console.debug()
+            $('#search_bar').removeClass('high');
+        } catch (e) {}
+    }
+}
+
+$(document).ready(() => {
+    $("#search_bar").on('keyup', checkResize);
+});
