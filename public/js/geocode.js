18fd71dd5d3bdbf3246b7189089a2b3590403c3d
Jake <pygamer138@gmail.com>
2020-03-21 18:43:40 +0100

    add geocoding

--- public/js/geocode.js
+++ public/js/geocode.js
@@ -2,6 +2,7 @@ geocoder = {};
 gh_token = 'f0697fa6-6b37-4a82-81b0-6dbf9170892c'; /*Plz dont kill me*/ 
 
 geocoder.geo = {
+    resCallback: null,
     /** 
      * @param {string} key: GraphHopper API key
     */
@@ -22,16 +23,54 @@ geocoder.geo = {
     /** 
      * @param {string} loc: The name of the location to search in the database
     */
-    getResults: function(loc) {
+    getResults: function(loc, callback) {
         var url = this.buildApiUrl(loc);
-        
+        var timedOut = false;
+
+        this.resCallback = callback;
+
+        var tm = setTimeout(() => {
+            timedOut = true;
+        }, 20000);
+        corslite(url, (err, res) => {
+            if (!timedOut) {
+				var fired = err ? err : res;
+                var r = {
+					status: fired.status,
+					limit: Number(fired.getResponseHeader("X-RateLimit-Limit")),
+					remaining: Number(fired.getResponseHeader("X-RateLimit-Remaining")),
+					reset: Number(fired.getResponseHeader("X-RateLimit-Reset")),
+					credits: Number(fired.getResponseHeader("X-RateLimit-Credits"))
+                }
+                console.log(`Limit: ${r.limit}\nRemaining: ${r.remaining}\nReset: ${r.reset}\nCredits: ${r.credits}`);
+                if (!err) {
+                    var data = JSON.parse(res.responseText);
+                    this._resultsDone(data);
+                }
+            }
+        }, true);
+    },
+    
+    _resultsDone: function(data) {
+        this.resCallback(data);
     }
 }
 
 const checkResize = () => {
     if ($('#search_input')[0].value !== '') {
         $('#search_bar').addClass('high');
-        console.log(geocoder.geo.getResults($('#search_input')[0].value));
+        geocoder.geo.getResults($('#search_input')[0].value, (data) => {
+            var e = $(".elementcontainer");
+            var es = e[0];
+            es.innerHTML = '';
+            for (let i = 0; i < data.hits.length; i++) {
+                let element = `<div class="place">
+                    <span class="name">${data.hits[i].name},</span><br/>
+                    <span class="country">${data.hits[i].country}</span>
+                </div>`;
+                es.innerHTML += element;
+            }
+        });
     } else {
         try {
             console.debug()
